e98fe5431c90b1cec12be3b4fb5d15f96811fde6
Julian Finelli <julianfinelli97@gmail.com>
2024-03-13 15:21:01 -0300

    modificacion

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,21 +10,21 @@ export default function Form() {
       <div className='w-5/6 h-auto max-sm:mt-0 max-sm:ml-0 ml-2 mt-4 flex justify-center max-sm:items-center max-sm:flex-col'>
         <div className=' max-sm:ml-0 flex flex-col'>
           <label htmlFor="input_nombre" className="block text-sm max-sm:ml-1  self-start font-medium text-gray-900 ">Nombre</label>
-          <input type="text" id="input_nombre" className="rounded block max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su nombre' required/>
+          <input type="text" id="input_nombre" className="rounded block max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500  dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su nombre' required/>
         </div>
         <div className=' flex flex-col max-sm:mt-3'>
           <label htmlFor="input_apellido" className="block text-sm max-sm:ml-1 ml-6 self-start font-medium text-gray-900 ">Apellido</label>
-          <input type="text" id="input_apellido" className="rounded block ml-6 max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su apellido' required/>
+          <input type="text" id="input_apellido" className="rounded block ml-6 max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su apellido' required/>
         </div>
       </div>
       <div className='w-5/6 h-auto max-sm:mt-0 mt-8 max-sm:ml-0 ml-2 flex justify-center max-sm:items-center max-sm:flex-col'>
         <div className=' max-sm:ml-0 flex flex-col'>
           <label htmlFor="input_nombre" className="block text-sm max-sm:ml-1  self-start font-medium text-gray-900 ">Nombre</label>
-          <input type="text" id="input_nombre" className="rounded block max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su nombre' required/>
+          <input type="text" id="input_nombre" className="rounded block max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su nombre' required/>
         </div>
         <div className=' flex flex-col max-sm:mt-3'>
           <label htmlFor="input_apellido" className="block text-sm max-sm:ml-1 ml-6 self-start font-medium text-gray-900 ">Apellido</label>
-          <input type="text" id="input_apellido" className="rounded block ml-6 max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su apellido' required/>
+          <input type="text" id="input_apellido" className="rounded block ml-6 max-sm:ml-1 max-sm:w-64 w-48 p-2 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:border-gray-600 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500 outline-none transition-all duration-300" placeholder='Ingrese su apellido' required/>
         </div>
       </div>
       <div className='w-5/6 h-20 flex justify-center items-center'>
